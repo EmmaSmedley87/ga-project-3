91c8d69612e374e30592955ffb65020f218fc106
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-09-30 21:27:11 +0100

    emmas change

--- db/placesSeedData.js
+++ db/placesSeedData.js
@@ -2,16 +2,19 @@ export const placesSeedData = [
     {
         Country: ,
         City: ,
+        DateVisitedc
         AnythingWeWant: ,
     },
     {
         Country: ,
         City: ,
+        DateVisited,
         AnythingWeWant: ,
     },
     {
         Country: ,
         City: ,
+        DateVisited,
         AnythingWeWant: ,
     }
 ]
